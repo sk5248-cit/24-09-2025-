8900edf029e4922740291392072c35172a6eeb50
sk5248-cit <sk5248@g.rit.edu>
2025-09-24 13:58:57 +0400

    Initial

--- 24-09-2025-mock.py
+++ 24-09-2025-mock.py
@@ -5,5 +5,21 @@ def checking_if_its_even_or_odd():
         return "Even"
     else:
         return "Odd"
-checking_if_its_even_or_odd()
+#Checking if its positive or negative number
+def checking_if_it_is_negative_or_positive(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "Zero"
+#Finding its square
+def square_of_the_number(num):
+    return num **2 
+#Finding its cube
+def cube_of_the_number(num):
+    return num **3
+#printing all the results
+def printing_reults(num):
+    print("the results we wanted",num)
 
